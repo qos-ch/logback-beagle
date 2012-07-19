1d24064d6384aef156914ed76df23f3671e86e8f
Ceki Gulcu <ceki@qos.ch>
2012-07-19 19:43:46 +0200

    added missing ViewClearActionDelegate

---
+++ ch.qos.logback.beagle/src/ch/qos/logback/beagle/actions/ViewClearActionDelegate.java
@@ -0,0 +1,35 @@
+package ch.qos.logback.beagle.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+import ch.qos.logback.beagle.views.BeagleView;
+import ch.qos.logback.beagle.vista.TableMediator;
+
+public class ViewClearActionDelegate implements IViewActionDelegate {
+
+  TableMediator tableMediator;
+  
+  @Override
+  public void run(IAction action) {
+    // TODO Auto-generated method stub
+    System.out.println("ViewClearActionDelegate.run "+action);
+    tableMediator.classicTISBuffer.removeAll();
+  }
+
+  @Override
+  public void selectionChanged(IAction action, ISelection selection) {
+    System.out.println("ViewClearActionDelegate.selectionChanged "+action+", selection:"+selection);
+
+  }
+
+  @Override
+  public void init(IViewPart view) {
+    System.out.println("ViewClearActionDelegate.init "+view);
+    BeagleView beagleView = (BeagleView) view;
+    tableMediator = beagleView.getTableMediator();
+  }
+
+}
