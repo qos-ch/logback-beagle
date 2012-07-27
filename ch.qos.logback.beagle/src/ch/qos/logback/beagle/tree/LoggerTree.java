394a5090c7009ca500f5a4df6797ec4455c3e76d
Ceki Gulcu <ceki@qos.ch>
2012-07-27 11:57:35 +0200

    Update the project version to 1.1.0.qualifier. Using logback-classic version 1.0.7-SNAPSHOT obtained from http://logback.qos.ch/p2/proper-nightly Starting work on logger tree

--- ch.qos.logback.beagle/src/ch/qos/logback/beagle/tree/LoggerTree.java
+++ ch.qos.logback.beagle/src/ch/qos/logback/beagle/tree/LoggerTree.java
@@ -1,15 +1,37 @@
 package ch.qos.logback.beagle.tree;
 
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.LoggerNameUtil;
 
 public class LoggerTree {
 
   final LoggerContext loggerContext;
+  final Tree tree;
   
-  LoggerTree(LoggerContext loggerContext) {
+  LoggerTree(LoggerContext loggerContext, Tree tree) {
     this.loggerContext = loggerContext;
+    this.tree = tree;
   }
   
   
+  void update(String loggerName) {
+    if(loggerContext.exists(loggerName) == null) {
+      // create the logger
+      Logger logger = loggerContext.getLogger(loggerName);
+      createTreeItemByLogger(logger);
+    }
+  }
+
+
+  private void createTreeItemByLogger(Logger logger) {
+    TreeItem[] rootItems = tree.getItems();
+    assert (rootItems.length != 1);
+    TreeItem rootItem = rootItems[0];
+    LoggerNameUtil.computeNameParts(logger.getName());
+  }
   
 }
