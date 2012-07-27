a349ebeedc16a52b197f2758d77dad331906380d
Ceki Gulcu <ceki@qos.ch>
2012-07-27 07:59:06 +0200

    started working on the logger tree

---
+++ ch.qos.logback.beagle/src/ch/qos/logback/beagle/tree/LoggerTree.java
@@ -0,0 +1,15 @@
+package ch.qos.logback.beagle.tree;
+
+import ch.qos.logback.classic.LoggerContext;
+
+public class LoggerTree {
+
+  final LoggerContext loggerContext;
+  
+  LoggerTree(LoggerContext loggerContext) {
+    this.loggerContext = loggerContext;
+  }
+  
+  
+  
+}
