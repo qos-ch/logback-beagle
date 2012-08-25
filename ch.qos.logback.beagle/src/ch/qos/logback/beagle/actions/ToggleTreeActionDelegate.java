2971cce43d8e408a8122b8b63c494aa7ae883a38
Ceki Gulcu <ceki@qos.ch>
2012-08-25 23:57:23 +0200

    formatting and addition of license header

--- ch.qos.logback.beagle/src/ch/qos/logback/beagle/actions/ToggleTreeActionDelegate.java
+++ ch.qos.logback.beagle/src/ch/qos/logback/beagle/actions/ToggleTreeActionDelegate.java
@@ -1,3 +1,11 @@
+/**
+ * Logback-beagle: The logback Console Plugin for Eclipse 
+ * Copyright (C) 2006-2012, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are licensed under
+ * the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
 package ch.qos.logback.beagle.actions;
 
 import org.eclipse.jface.action.IAction;
@@ -19,45 +27,44 @@ import ch.qos.logback.beagle.view.TableMediator;
  */
 public class ToggleTreeActionDelegate implements IViewActionDelegate {
 
-	private BeagleView beagleView = null;
-	private FormAttachment oldLeft = null;
-	private FormAttachment oldRight = null;
-	private boolean hidden = false;
+  private BeagleView beagleView = null;
+  private FormAttachment oldLeft = null;
+  private FormAttachment oldRight = null;
+  private boolean hidden = false;
 
-	@Override
-	public void init(IViewPart view) {
-		if (view instanceof BeagleView) {
-			beagleView = (BeagleView) view;
-		}
-	}
+  @Override
+  public void init(IViewPart view) {
+    if (view instanceof BeagleView) {
+      beagleView = (BeagleView) view;
+    }
+  }
 
-	@Override
-	public void run(IAction action) {
-		if (beagleView != null) {
-			TableMediator tableMediator = beagleView.getTableMediator();
-			if (tableMediator != null) {
-				Sash sash = tableMediator.getSash();
-				FormData formData = (FormData) sash.getLayoutData();
-				if (hidden) {
-					hidden = false;
-					formData.left = oldLeft;
-					formData.right = oldRight;
-				} else {
-					hidden = true;
-					oldLeft = formData.left;
-					oldRight = formData.right;
-					formData.left = new FormAttachment(0, 0);
-					formData.right = new FormAttachment(0,
-							0 + Constants.SASH_WIDTH);
-				}
-				sash.getParent().layout();
-			}
-		}
-	}
+  @Override
+  public void run(IAction action) {
+    if (beagleView != null) {
+      TableMediator tableMediator = beagleView.getTableMediator();
+      if (tableMediator != null) {
+        Sash sash = tableMediator.getSash();
+        FormData formData = (FormData) sash.getLayoutData();
+        if (hidden) {
+          hidden = false;
+          formData.left = oldLeft;
+          formData.right = oldRight;
+        } else {
+          hidden = true;
+          oldLeft = formData.left;
+          oldRight = formData.right;
+          formData.left = new FormAttachment(0, 0);
+          formData.right = new FormAttachment(0, 0 + Constants.SASH_WIDTH);
+        }
+        sash.getParent().layout();
+      }
+    }
+  }
 
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// nothing to do
-	}
+  @Override
+  public void selectionChanged(IAction action, ISelection selection) {
+    // nothing to do
+  }
 
 }
--- ch.qos.logback.beagle/src/ch/qos/logback/beagle/actions/ViewClearActionDelegate.java
+++ ch.qos.logback.beagle/src/ch/qos/logback/beagle/actions/ViewClearActionDelegate.java
@@ -1,3 +1,11 @@
+/**
+ * Logback-beagle: The logback Console Plugin for Eclipse 
+ * Copyright (C) 2006-2012, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are licensed under
+ * the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
 package ch.qos.logback.beagle.actions;
 
 import org.eclipse.jface.action.IAction;
